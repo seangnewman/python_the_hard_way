f9b6c461d0d0e1c0050c150670f4ad40d46b4317
Sean Newman <seangnewman@msn.com>
2018-08-18 10:11:32 -0700

    through exercise 5

---
+++ ex5.py
@@ -0,0 +1,17 @@
+my_name = "Sean Newman"
+my_age = 56
+my_height = 70 * 2.54 #centimeters
+my_weight = 260 * 0.453592 #kilograms
+my_eyes = "Brown"
+my_teeth = "white"
+my_hair = "Black"
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d pounds heavy." % my_weight
+print "Actually that's pretty heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+# This line is tricky, try to get it exactly right
+print "If I add %d, %d, and %d I get %d. " % (my_age, my_height, my_weight, my_age + my_height + my_weight)
