13683fbd5af575b55a1c9907e96b0dc8a270a33e
Sean Newman <seangnewman@msn.com>
2018-09-07 07:47:09 -0700

    through relationships

---
+++ ex34.py
@@ -0,0 +1,13 @@
+animals = ['bear', 'python','peacock', 'kangaroo', 'whale','platypus']
+
+for animal in range(0, len(animals) ):
+  print "The animal at %d is %s" % (animal, animals[animal])
+
+print "The animal at 1 is %s" % animals[1]
+print "The third animal is %s" % animals[2]
+print "The first animal is %s" % animals[0]
+print "The animal at 3 is %s" % animals[3]
+print "The fifth (5th) animal is %s" % animals[4]
+print "The animal at 2 is %s" % animals[2]
+print "The sixth (6th) animal is %s" % animals[5]
+print "The animal at 4 is %s" % animals[4]
